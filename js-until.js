1e1ee5e1e1040e3c703225aea6a3537c89db56f0
zhigao miao <mzg@zhigaodeMacBook-Pro.local>
2018-03-03 19:48:51 +0800

    add class operation

--- js-until.js
+++ js-until.js
@@ -51,8 +51,44 @@
         },
         forEach: function(fn) {
             return $(fn);
-        }
+        },
+	get: function(index) {
+        return $.dom[index||0];
+	},
+	addClass: function(className) {
+	    return $(function(el) {
+			el.classList.add(className);
+		});
+	},
+	removeClass: function(className) {
+		return $(function(el){
+			el.classList.remove(className);
+		});
+	},
+	hasClass: function(className) {
+		return $.fn.get(0).classList.contains(className);
+	},
+	css: function(style) {
+    	return $(function(el) {
+    		el.style.cssText += ';' + style; 
+		});
+	},
+	remove: function() {
+	},
+	append: function() {},
+	prepend: function() {},
+	after: function() {},
+	before: function() {},
+	empty: function() {},
+	anim: function() {},
     }
+    (function(){
+		function ajax() {
+		};
+        
+	}());
+    var adj_ops = {append: 'beforeEnd', prepend: 'afterBegin', before: 'beforeBegin', after: 'afterEnd'};	
+    
     $.fn.dom = $.dom;
     $.prototype = $.fn;
     return $;
