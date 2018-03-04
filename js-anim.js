1e2285dc06c866121f9a053afc2a4596419780a0
mzg <67715107@qq.com>
2018-03-04 18:45:41 +0800

    add simple animation

---
+++ js-anim.js
@@ -0,0 +1,188 @@
+(
+    function(name, context, definition) {
+        'use strict';
+        if(typeof module !== 'undefined' && module.exports) {
+            module.exports = definition();
+        } else if(typeof define === 'function' && define.amd) {
+            define(definition);
+        } else {
+            context[name] = definition();
+        }
+    }
+)('jsAnim', this, function() {
+    var Tween = {
+		linear: function(t,b,c,d){ return c*t/d + b; },
+		easeInQuad: function(t,b,c,d){
+			return c*(t/=d)*t + b;
+		},
+		easeOutQuad: function(t,b,c,d){
+			return -c *(t/=d)*(t-2) + b;
+		},
+		easeInOutQuad: function(t,b,c,d){
+			if ((t/=d/2) < 1) return c/2*t*t + b;
+			return -c/2 * ((--t)*(t-2) - 1) + b;
+		},
+		easeInCubic: function(t,b,c,d){
+			return c*(t/=d)*t*t + b;
+		},
+		easeOutCubic: function(t,b,c,d){
+			return c*((t=t/d-1)*t*t + 1) + b;
+		},
+		easeInOutCubic: function(t,b,c,d){
+			if ((t/=d/2) < 1) return c/2*t*t*t + b;
+			return c/2*((t-=2)*t*t + 2) + b;
+		},
+		easeInQuart: function(t,b,c,d){
+			return c*(t/=d)*t*t*t + b;
+		},
+		easeOutQuart: function(t,b,c,d){
+			return -c * ((t=t/d-1)*t*t*t - 1) + b;
+		},
+		easeInOutQuart: function(t,b,c,d){
+			if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+			return -c/2 * ((t-=2)*t*t*t - 2) + b;
+		},
+		easeInQuint: function(t,b,c,d){
+			return c*(t/=d)*t*t*t*t + b;
+		},
+		easeOutQuint: function(t,b,c,d){
+			return c*((t=t/d-1)*t*t*t*t + 1) + b;
+		},
+		easeInOutQuint: function(t,b,c,d){
+			if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+			return c/2*((t-=2)*t*t*t*t + 2) + b;
+		},
+		easeInSine: function(t,b,c,d){
+			return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+		},
+		easeOutSine: function(t,b,c,d){
+			return c * Math.sin(t/d * (Math.PI/2)) + b;
+		},
+		easeInOutSine: function(t,b,c,d){
+			return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+		},
+		easeInExpo: function(t,b,c,d){
+			return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+		},
+		easeOutExpo: function(t,b,c,d){
+			return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+		},
+		easeInOutExpo: function(t,b,c,d){
+			if (t==0) return b;
+			if (t==d) return b+c;
+			if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+			return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+		},
+		easeInCirc: function(t,b,c,d){
+			return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+		},
+		easeOutCirc: function(t,b,c,d){
+			return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+		},
+		easeInOutCirc: function(t,b,c,d){
+			if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+			return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+		},
+		easeInElastic: function(t,b,c,d,a,p){
+			if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+			if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+			else var s = p/(2*Math.PI) * Math.asin (c/a);
+			return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+		},
+		easeOutElastic: function(t,b,c,d,a,p){
+			if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+			if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+			else var s = p/(2*Math.PI) * Math.asin (c/a);
+			return (a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b);
+		},
+		easeInOutElastic: function(t,b,c,d,a,p){
+			if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+			if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+			else var s = p/(2*Math.PI) * Math.asin (c/a);
+			if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+			return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+		},
+		easeInBack: function(t,b,c,d,s){
+			if (s == undefined) s = 1.70158;
+			return c*(t/=d)*t*((s+1)*t - s) + b;
+		},
+		easeOutBack: function(t,b,c,d,s){
+			if (s == undefined) s = 1.70158;
+			return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+		},
+		easeInOutBack: function(t,b,c,d,s){
+			if (s == undefined) s = 1.70158; 
+			if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+			return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+		},
+		easeInBounce: function(t,b,c,d){
+			return c - Tween.easeOutBounce(d-t, 0, c, d) + b;
+		},
+		easeOutBounce: function(t,b,c,d){
+			if ((t/=d) < (1/2.75)) {
+				return c*(7.5625*t*t) + b;
+			} else if (t < (2/2.75)) {
+				return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+			} else if (t < (2.5/2.75)) {
+				return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+			} else {
+				return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+			}
+		},
+		easeInOutBounce: function(t,b,c,d){
+			if (t < d/2) return Tween.easeInBounce(t*2, 0, c, d) * .5 + b;
+			else return Tween.easeOutBounce(t*2-d, 0, c, d) * .5 + c*.5 + b;
+		}
+		}
+	var $a = function(opt) {
+		return new animC(opt);
+	};
+	function _init(value) {
+		var result = [];
+		value.forEach(function(item){
+			result.push({
+				start: parseFloat(item[0]),
+				end: parseFloat(item[1]),
+			});
+		});
+		return result;
+	};
+	function _defaultRenderFun() {};
+	function animC(opt) {
+		this.statue = 'init';
+		this.value = _init.call(this, opt.value);
+		this.duration = opt.duration;
+		this.timeFun = opt.timeFun || 'linear';
+		this.renderFun = opt.renderFun || _defaultRenderFun.bind(this);
+	};
+	function _renderFun(curT, dT,tF) {
+		var tempV = this.value.map(function(item) {
+			return tF(curT, item.start, item.end, dT);
+		});
+		this.renderFun(tempV);
+	}
+	function _loop() {
+		var curT = Date.now() - this.beginTime;
+		var dT = this.duration;
+		var tF = Tween[this.timeFun];
+        if (curT >= dT) {
+			this.state = 'end';
+            _renderFun.call(this, dT,dT,tF);
+		} else {
+			_renderFun.call(this, curT,dT,tF);
+			window.requestAnimationFrame(_loop.bind(this));
+		}
+	};
+	function _play() {
+		this.state = 'play';
+		this.beginTime = Date.now();
+		window.requestAnimationFrame(_loop.bind(this));
+	};
+    animC.prototype = {
+		play: function() {
+			_play.bind(this)();
+		},
+	};
+	
+	return $a;	
+});
--- js-until.js
+++ js-until.js
@@ -15,12 +15,19 @@
         if(typeof any === 'function') {
             $.dom.forEach(any);
         } else {
+			$._ = any;
             $.dom = [].slice.apply(document.querySelectorAll(any));
             $.fn.dom = $.dom;
         }
         return $.fn;
     }
-
+	function deleteDom(el) {
+		var index = $.dom.indexOf(el);
+        console.log(index);
+		if (index > -1) {
+			$.dom.splice(index, 1);
+		}
+	};
     $.fn = {
         on: function(type, handler, useCapture) {
             useCapture === undefined || (useCapture = false);
@@ -46,9 +53,6 @@
                 }
             });
         },
-        length: (function() {
-            return $.dom.length;
-        })(),
         forEach: function(fn) {
             return $(fn);
         },
@@ -66,6 +70,9 @@
 			});
 		},
 		hasClass: function(className) {
+			if (this.length === 0) {
+				return false;
+			}
 			return $.fn.get(0).classList.contains(className);
 		},
 		css: function(style) {
@@ -74,23 +81,55 @@
 			});
 		},
 		remove: function() {
+			$(function(el) {
+				el.parentNode.removeChild(el);
+			});
+			return $($._);
 		},
-		append: function() {},
-		prepend: function() {},
-		after: function() {},
-		before: function() {},
-		empty: function() {},
+		empty: function() {
+			return $(function(el) {
+				el.innerHTML = '';
+			});
+		},
 		anim: function() {},
     };
+	Object.defineProperty($.fn, 'length', {
+    	get: function() {
+			return $.dom.length;
+    	}
+    });
     (function(){
-		function ajax() {
-			console.log(3);
+		function ajax(method, url, success, error) {
+			var x = new XMLHttpRequest();
+			x.onreadystatechange = function(){
+				console.log(x);
+				if(x.readyState == 4 && (x.status == 200 || x.status == 0)) {
+					success(x.responseText);
+				} else {
+				}
+			};
+			x.open(method,url,true);
+			x.send(null);
+		};
+
+		$.get = function(url, success){ ajax('GET', url, success); };
+		$.post = function(url, success){ ajax('POST', url, success); };
+		$.getJSON = function(url, success){
+			$.get(url, function(json){ success(JSON.parse(json)) });
 		};
         
 	})();
     var adj_ops = {append: 'beforeEnd', prepend: 'afterBegin', before: 'beforeBegin', after: 'afterEnd'};	
+	for(var key in adj_ops) {
+		$.fn[key] = (function(key){
+			return function(html){
+				return $(function(el){
+					el.insertAdjacentHTML(key, html);
+				})			
+			}
+		})(adj_ops[key]);
+	}
     
-    $.fn.dom = $.dom;
     $.prototype = $.fn;
     return $;
 });
