f89be173ef772601ee44b74bf11dab386d4bc883
mzg <67715107@qq.com>
2018-03-04 22:13:51 +0800

    add simple ajax

--- js-until.js
+++ js-until.js
@@ -98,27 +98,120 @@
 			return $.dom.length;
     	}
     });
+	//ajax start
     (function(){
-		function ajax(method, url, success, error) {
-			var x = new XMLHttpRequest();
-			x.onreadystatechange = function(){
-				console.log(x);
-				if(x.readyState == 4 && (x.status == 200 || x.status == 0)) {
-					success(x.responseText);
-				} else {
+          $.ajaxSettings = {
+			type: 'GET',
+			beforeSend: empty,
+			success: empty,
+			error: empty,
+			complete: empty,
+			context: null,
+			// Whether to trigger "global" Ajax events
+			global: true,
+			xhr: function () {
+			  return new window.XMLHttpRequest()
+			},
+			accepts: {
+			  script: 'text/javascript, application/javascript, application/x-javascript',
+			  json:   'application/json',
+			  xml:    'application/xml, text/xml',
+			  html:   'text/html',
+			  text:   'text/plain'
+			},
+			crossDomain: false,
+			timeout: 0,
+			processData: true,
+			cache: true,
+			dataFilter: empty
+		  }
+		function empty() {};
+		function ajax(option) {
+			var settings = Object.assign($.ajaxSettings, option);
+			var dataType = settings.dataType;
+			if (dataType === 'jsonp') {
+				console.log('jsonp is sending');
+			}
+			
+			var mime = settings.accepts[dataType],
+				headers = { },
+				setHeader = function(name, value) { headers[name.toLowerCase()] = [name, value] },
+				protocol = /^([\w-]+:)\/\//.test(settings.url) ? RegExp.$1 : window.location.protocol,
+				xhr = settings.xhr(),
+				nativeSetHeader = xhr.setRequestHeader;
+
+			if (!settings.crossDomain) setHeader('X-Requested-With', 'XMLHttpRequest');
+			setHeader('Accept', mime || '*/*');
+			if (mime = settings.mimeType || mime) {
+			  if (mime.indexOf(',') > -1) mime = mime.split(',', 2)[0]
+			  xhr.overrideMimeType && xhr.overrideMimeType(mime)
+			};
+			if (settings.contentType ||
+				(settings.contentType !== false && settings.data && settings.type.toUpperCase() != 'GET')){
+			  setHeader('Content-Type', settings.contentType || 'application/x-www-form-urlencoded')
+			};
+
+			if (settings.headers) {
+				for (name in settings.headers) {
+					setHeader(name, settings.headers[name]);
 				}
 			};
-			x.open(method,url,true);
-			x.send(null);
+			xhr.setRequestHeader = setHeader;
+			xhr.onreadystatechange = function(){
+			  if (xhr.readyState == 4) {
+				xhr.onreadystatechange = empty
+				var result, error = false
+				if ((xhr.status >= 200 && xhr.status < 300)
+						|| xhr.status == 304
+						|| (xhr.status == 0 && protocol == 'file:')) {
+
+				  if (xhr.responseType == 'arraybuffer' || xhr.responseType == 'blob')
+					result = xhr.response
+				  else {
+					result = xhr.responseText
+					try {
+					  if (dataType == 'xml') {
+						result = xhr.responseXML;
+				      } else if (dataType == 'json') {
+							result = JSON.parse(result);
+					  };
+					} catch (e) { error = e }
+
+					if (error){
+						settings.error.call(settings.context,xhr,settings);
+                    }
+				  }
+				  settings.success.call(settings.context,result,xhr,settings);
+				} else {
+				    settings.error.call(settings.context,xhr,settings);
+				}
+			  };
+			};
+			xhr.open(settings.type, settings.url, true);
+			xhr.send(settings.data ? settings.data : null);
+			
 		};
 
-		$.get = function(url, success){ ajax('GET', url, success); };
-		$.post = function(url, success){ ajax('POST', url, success); };
+		$.get = function(option){
+			/*var option = {
+				url: url,
+				data: data,
+				success: success,
+				dataType: dataType
+			};*/
+			ajax(option);
+		};
+		$.post = function(option){
+			 option.type = 'POST';
+			 ajax(option); 
+
+		};
 		$.getJSON = function(url, success){
 			$.get(url, function(json){ success(JSON.parse(json)) });
 		};
         
 	})();
+	//ajax end
     var adj_ops = {append: 'beforeEnd', prepend: 'afterBegin', before: 'beforeBegin', after: 'afterEnd'};	
 	for(var key in adj_ops) {
 		$.fn[key] = (function(key){
