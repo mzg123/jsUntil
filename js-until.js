cfaca87611902c7cafb3099a4c23e86c8bc71e87
miaozhigao <miaozhigao@babytree-inc.com>
2018-03-14 15:27:57 +0800

    touch 相关代码 下移到fn下面

--- js-until.js
+++ js-until.js
@@ -59,42 +59,6 @@
         return 'tagName' in node ? node : node.parentNode;
     }
 
-    var touch = {}, touchTimeout;
-    document.ontouchstart = function(e) {
-        var now = Date.now();
-		touch.target = parentIfText(e.touches[0].target);
-        var delta = now - (touch.last || now);
-        touch.x1 = e.touchs[0].pageX;
-        touchTimeout && clearTimeout(touchTimeout);
-	
-        if (delta > 0 && delta < 250) {
-	    touch.isDoubleTap = true;
-        } else {
-            touch.last = now;
-        }
-    }
-    document.ontouchmove = function(e) {
-        touch.x2 = e.touches[0].pageX;
-    }
-    document.ontouchend = function(e) {
-	if (touch.isDoubleTap) {
-            dispatch('doubleTap', touch.target);
-	    touch = {};
-        }
-        else if (touch.x2 > 0) {
-            touch.x1 - touch.x2 > 30 && dispatch('swipeLeft', touch.target);
-            touch.x1 -touch.x2 < -30 && dispatch('swipeRight', touch.target);
-        } else if ('last' in touch) {
-            touchTimeout = setTimeout(function(){
-                touchTimeout = null;
-                dispatch('tap', touch.target);
-                touch = {};
-            }, 250);
-        }
-    }
-    document.ontouchcancel = function(e) {
-        touch = {};
-    }
     $.fn = {
         on: function(type, handler, useCapture) {
             useCapture === undefined || (useCapture = false);
@@ -289,9 +253,55 @@
 		})(adj_ops[key]);
 	}
     ['width','height'].forEach(function(m){ $.fn[m] = function(){ return this.offset()[m] }});
-    ['swipeLeft', 'swipeRight', 'doubleTap', 'tap'].forEach(function(m){
-        $.fn[m] = function(callback) { return this.bind(m, callback);}
+    ['swipe', 'swipeLeft', 'swipeRight', 'doubleTap', 'tap'].forEach(function(m){
+        $.fn[m] = function(callback) { return $.fn.bind(m, callback);}
     });
+
+
+    var touch = {}, touchTimeout;
+    var down = function(e) {
+        var now = Date.now();
+		touch.target = parentIfText(e.touches[0].target);
+        var delta = now - (touch.last || now);
+        touch.x1 = e.touches[0].pageX;
+        touchTimeout && clearTimeout(touchTimeout);
+	
+        if (delta > 0 && delta < 250) {
+	    touch.isDoubleTap = true;
+        } else {
+            touch.last = now;
+        }
+    }
+    var move = function(e) {
+        touch.x2 = e.touches[0].pageX;
+    }
+    var up = function(e) {
+	if (touch.isDoubleTap) {
+            dispatch('doubleTap', touch.target);
+	    touch = {};
+        }
+        else if (touch.x2 > 0) {
+            touch.x1 - touch.x2 > 30 && (dispatch('swipe', touch.target) || dispatch('swipeLeft', touch.target));
+            touch.x1 -touch.x2 < -30 && (dispatch('swipe', touch.target) || dispatch('swipeRight', touch.target));
+        } else if ('last' in touch) {
+            touchTimeout = setTimeout(function(){
+                touchTimeout = null;
+                dispatch('tap', touch.target);
+                touch = {};
+            }, 250);
+        }
+    }
+    var cancel = function(e) {
+        touch = {};
+    }
+    var eventMap = {
+        down: 'touchstart',
+        up: 'touchend',
+        move: 'touchmove'
+    }
+    $('body').on(eventMap.down, down)
+        .on(eventMap.up, up)
+        .on(eventMap.move, move);
 	//ajax start
     (function(){
           $.ajaxSettings = {
